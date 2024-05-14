73def83e0e2af774660cc1800a61167d4f31c697
Kevin Salinas <ksalinas191@gmail.com>
2024-05-14 12:20:11 -0300

    update 4

---
+++ Ejercicio 11,12,13.hs
@@ -0,0 +1,17 @@
+-- ?Ejercicio 11
+-- *Construir un programa no recursivo que realice la suma de números complejos, los cuales se ingresan en sublistas con pares de números donde el primer elemento es la componente real y el segundo la componente imaginaria
+
+sumComplex :: [(Float, Float)] -> (Float, Float)
+sumComplex = foldl (\(sumR, sumI) (r, i) -> (r + sumR, i + sumI)) (0, 0)
+
+-- ?Ejercicio 12
+-- * Dada una lista ordenada y un átomo escribir una función que inserte el átomo en el lugar correspondiente
+
+insertAtom :: Ord a => [a] -> a -> [a]
+insertAtom [] x = [x]
+insertAtom (x:xs) y
+  | x < y = x : insertAtom xs y
+  | otherwise = y : x : xs
+
+-- ?Ejercicio 13
+-- * Calcular la suma de dos matrices.
