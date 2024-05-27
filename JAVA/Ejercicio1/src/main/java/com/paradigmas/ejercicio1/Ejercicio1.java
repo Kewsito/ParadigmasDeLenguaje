8b1aa50690a5e307d092d1345e0620b332a5e481
Kevin Salinas <ksalinas191@gmail.com>
2024-05-27 16:03:31 -0300

    update java

--- JAVA/Ejercicio1/src/main/java/com/paradigmas/ejercicio1/Ejercicio1.java
+++ JAVA/Ejercicio1/src/main/java/com/paradigmas/ejercicio1/Ejercicio1.java
@@ -3,7 +3,11 @@
  */
 
 package com.paradigmas.ejercicio1;
+import com.paradigmas.ejercicio1.claseGestor;
 
+import java.util.Scanner;
+
+import com.paradigmas.ejercicio1.ViajeroFrecuente;
 /**
  *
  * @author Kewsito
@@ -11,6 +15,21 @@ package com.paradigmas.ejercicio1;
 public class Ejercicio1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        claseGestor gestor = new claseGestor(10);
+        
+        int opc = -1;
+        while (opc!=0) {
+            System.out.println("1. Agregar viajero");
+            System.out.println("2. Mostrar viajero");
+            System.out.println("3. Consultar millas");
+            System.out.println("4. Acumular Millas");
+            System.out.println("5. Canjear Millas");
+            System.out.println("6. Mejor viajero");
+            System.out.println("0. Salir");
+            
+            Scanner op = new Scanner(System.in);
+            opc = op.nextInt();
+            op.close();
+        }
     }
 }
